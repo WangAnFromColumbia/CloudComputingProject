6aff85955fc85e5cdc806f4692e0b22bd688038c
Mohneesh Patel <mp3542@columbia.edu>
2017-09-21 20:14:13 -0400

    Update index.js

--- email-lambda/index.js
+++ email-lambda/index.js
@@ -1,13 +1,13 @@
 var ses = require('node-ses');  
-var ses_client = ses.createClient({ key: 'AKIAIOHNP33JNUJN2CKA', secret: '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' });  
+var ses_client = ses.createClient({ key: 'AWS_KEY', secret: 'AWS_KEY' });  
 
   exports.handler = (event, context, callback) => {
     // TODO implement
         
       // Give SES the details and let it construct the message for you.   
       ses_client.sendEmail({  
-      to: 'aw3001@columbia.edu'  
-      , from: 'weiyimingege@gmail.com'  
+      to: 'Email_1'  
+      , from: 'Email_2'  
       , subject: 'Coupon'  
       , message: "You got a coupon" 
       , altText: 'plain text'  
